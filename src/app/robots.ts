dd51772a379a563902d9cc3b33bd5bb9b55e042e
sitarammaaker-arch <sitaram.maaker@gmail.com>
2026-03-01 22:38:04 +0530

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/api/',
     },
-    sitemap: 'https://govphoto.in/sitemap.xml',
+    sitemap: 'https://signresizer.com/sitemap.xml',
   };
 }
