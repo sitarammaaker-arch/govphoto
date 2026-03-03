b90f56752b977b0c6ace204c11415c1f9a6dac64
sitarammaaker-arch <sitaram.maaker@gmail.com>
2026-03-04 01:16:41 +0530

    Update sanity.config.ts

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,19 +1,14 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
-import { visionTool } from '@sanity/vision'
 import { schemaTypes } from './src/sanity/schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'SignResizer Blog',
-
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'your-project-id',
   dataset: 'production',
-
   basePath: '/studio',
-
-  plugins: [structureTool(), visionTool()],
-
+  plugins: [structureTool()],
   schema: {
     types: schemaTypes,
   },
